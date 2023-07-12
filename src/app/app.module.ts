cbe1b84efdebbeae07ca4316ef2809c6dfcd4c3d
josanma <j.sanma@hotmail.com>
2023-07-11 20:24:55 -0400

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {Media} from "@awesome-cordova-plugins/media/ngx";
+
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Media],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Platform } from '@ionic/angular';
+import { Media, MediaObject } from '@awesome-cordova-plugins/media/ngx';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +8,25 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  mediaObject!: MediaObject;
+
+  constructor(private platform: Platform, private media: Media) {
+    platform.ready().then(() => {
+      this.createMediaObject();
+    });
+  }
+
+  createMediaObject() {
+    console.log('createMediaObject')
+    this.mediaObject = this.media.create('https://streaming.comunicacioneschile.net/8002/;');
+    this.mediaObject.onSuccess.subscribe(() => console.log('Action is successful'));
+    this.mediaObject.onError.subscribe(error => console.log('Error!', error));
+    this.mediaObject.play()
+  }
+
+  playAudio() {
+    console.log('playAudio')
+    this.mediaObject.play();
+  }
 
 }
